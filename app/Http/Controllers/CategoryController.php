d45d5c1127e414cd048a69bd047cca97d070bd81
kamau simon <kamausimon217@gmail.com>
2024-07-05 00:48:33 +0300

    Refactor CategoryController to use ProductCategory model and update sidebar view

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,6 +11,7 @@ class CategoryController extends Controller
     public function show()
     {
         $categories = ProductCategory::WhereNull('parent_id')->with('subCategories')->get(); //get all the categories
+        dd($categories); //dump the categories
         return view('partials._sidebar', compact('categories')); //return the view with the categories
     }
 }
