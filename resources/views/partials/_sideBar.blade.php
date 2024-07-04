d7d70b405fc1dc0fd3dc4528faf805fd95c8e77b
kamau simon <kamausimon217@gmail.com>
2024-07-05 00:45:09 +0300

    Refactor sidebar view to improve readability and consistency

--- resources/views/partials/_sideBar.blade.php
+++ resources/views/partials/_sideBar.blade.php
@@ -1,30 +1,24 @@
-      {{dd($categories)}}
-      <div class="lg:fixed w-64  p-0 m-0 h-screen bg-white">
-
-          <a href="{{route('dashboard.index')}} " class="mt-3">
-              <img class="block mt-4" src="/images/shope-high-resolution-logo-transparent.png" alt="logo" />
-
-              <div>
-                  <h2 class="text-xl font-semibold text-gray-700">Categories</h2>
-                  <ul class="mt-4">
-                      @foreach($categories as $category)
-                      <li>
-                          {{$category->name}}
-                          @if($category->subcategories->count()> 0)
-                          <ul>
-                              @foreach($category -> $subcategories as $subcategory)
-                              <li>
-                                  {{$subcategory->name}}
-                              </li>
-                              @endforeach
-                          </ul>
-                          @endif
-                      </li>
-                      @endforeach
-                  </ul>
-              </div>
-          </a>
-
-
-
-      </div>
+<div class="lg:fixed w-64 p-0 m-0 h-screen bg-white">
+    <a href="{{ route('dashboard.index') }}" class="mt-3">
+        <img class="block mt-4" src="/images/shope-high-resolution-logo-transparent.png" alt="logo" />
+        <div>
+            <h2 class="text-xl font-semibold text-gray-700">Categories</h2>
+            <ul class="mt-4">
+                @foreach($categories as $category)
+                <li>
+                    {{ $category->name }}
+                    @if($category->subcategories->count() > 0)
+                    <ul>
+                        @foreach($category->subcategories as $subcategory)
+                        <li>
+                            {{ $subcategory->name }}
+                        </li>
+                        @endforeach
+                    </ul>
+                    @endif
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </a>
+</div>
