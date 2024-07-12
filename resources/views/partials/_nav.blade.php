6b04f67e1d575e1c1202d751b595440de9344b06
kamau simon <kamausimon217@gmail.com>
2024-07-13 02:41:30 +0300

    Refactor navigation bar to include categories and improve styling

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -2,7 +2,7 @@
     <a href="{{route('landing.index')}}">
         <img class="block w-24 h-auto ml-6" src="/images/shope-high-resolution-logo-transparent.png" alt="logo" />
     </a>
-
+    @if(request()->routeIs('landing.index'))
     <a href="#" class="text-white">Categories</a>
     <a href="#" class="text-white">What's new</a>
 
@@ -12,9 +12,10 @@
             <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700">Search</button>
         </form>
     </div>
-
+    @endif
     <div>
         <div class="flex gap-4">
+            @if(request()->routeIs('landing.index'))
             <a href="{{ route('cart.index') }}" class="flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-100 text-white">
                 Cart
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 512 512" fill="white">
@@ -23,6 +24,7 @@
                     <path d="M167.78 304h261.34l38.4-192H133.89l-8.47-48H32v32h66.58l48 272H432v-32H173.42z" />
                 </svg>
             </a>
+            @endif
             @if(!request()->routeIs('Auth.login'))
             <a href="{{ route('Auth.login') }}" class=" ml-4 mr-2 flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-100 text-white">
                 Login
