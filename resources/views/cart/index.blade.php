d5fd1fadc33bc7585032bab3f135a6b53ede7478
kamau simon <kamausimon217@gmail.com>
2024-07-26 20:55:54 +0300

    refactor: Update cart index page layout and functionality

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -10,8 +10,8 @@
     <!-- data div -->
     <div class="flex flex-col items-center mt-40">
         <a href="{{route('dashboard.index')}}" class="ml-4">
-            <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 16 16">
-                <path fill="grey" fill-rule="evenodd" d="m2.87 7.75l1.97 1.97a.75.75 0 1 1-1.06 1.06L.53 7.53L0 7l.53-.53l3.25-3.25a.75.75 0 0 1 1.06 1.06L2.87 6.25h9.88a3.25 3.25 0 0 1 0 6.5h-2a.75.75 0 0 1 0-1.5h2a1.75 1.75 0 1 0 0-3.5z" clip-rule="evenodd" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                <path fill="grey" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8z" />
             </svg>
         </a>
         <div class="w-full max-w-4xl">
