2e2d8520c02aa454e6cac6ecd9a1f9e6878c8936
kamau simon <kamausimon217@gmail.com>
2024-08-02 11:22:54 +0300

    refactor: Update admin sidebar navigation styling and structure

--- resources/views/partials/_adminSidebar.blade.php
+++ resources/views/partials/_adminSidebar.blade.php
@@ -1,9 +1,11 @@
-<div class="lg:fixed w-64 p-0 m-0 h-screen bg-gray-100">
+<div class="lg:fixed w-64 p-0 m-0 h-screen bg-gray-100 flex flex-col justify-between">
+    <!-- Logo Section -->
     <a href="{{ route('admin.index') }}" class="mt-3">
         <img class="block mt-4" src="/images/shope-high-resolution-logo-transparent.png" alt="logo" />
     </a>
 
-    <div class="relative mt-24 flex flex-col space-y-4">
+    <!-- Navigation Section -->
+    <div class="relative  flex flex-col space-y-8">
         <button class="text-grey hover:bg-gray-200 px-4 py-2 rounded-md focus:outline-none" id="productsDropdownButton">
             Products
         </button>
@@ -11,11 +13,11 @@
             <a href="{{ route('Products.index') }}" class="block px-4 py-2 text-grey hover:bg-gray-200">Products List</a>
             <a href="{{ route('Products.create') }}" class="block px-4 py-2 text-grey hover:bg-gray-200">Create Product</a>
         </div>
-        <button class=" text-grey hover:bg-gray-200 px-4 py-2 rounded-md focus:outline-none">
+        <button class="text-grey hover:bg-gray-200 px-4 py-2 rounded-md focus:outline-none">
             Sales
         </button>
         <button class="text-grey hover:bg-gray-200 px-4 py-2 rounded-md focus:outline-none">
-            customers
+            Customers
         </button>
         <button class="text-grey hover:bg-gray-200 px-4 py-2 rounded-md focus:outline-none">
             Analytics
@@ -23,22 +25,19 @@
         <button class="text-grey hover:bg-gray-200 px-4 py-2 rounded-md focus:outline-none">
             Notifications
         </button>
-
     </div>
 
-    <form action="{{ route('Auth.logout') }}" method="POST" class="px-4 py-2 ">
+    <!-- Logout Form Section -->
+    <form action="{{ route('Auth.logout') }}" method="POST" class="px-4 py-2 mx-11 mb-14">
         @csrf
-        <div class="flex space-x-1 ">
+        <div class="flex space-x-1 items-center">
             <span class="ml-2">Logout</span>
             <svg class="mt-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
                 <path fill="grey" d="M5 5h7V3H3v18h9v-2H5z" />
                 <path fill="grey" d="m21 12l-4-4v3H9v2h8v3z" />
             </svg>
         </div>
-
-
     </form>
-
 </div>
 
 <script>
