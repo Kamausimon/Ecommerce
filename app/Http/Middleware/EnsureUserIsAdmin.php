9ca454a8a406ba7bab97537ad6040f6d641bfa96
kamau simon <kamausimon217@gmail.com>
2024-08-05 13:24:49 +0300

    refactor: Update EnsureUserIsAdmin middleware to handle authentication and admin access

--- app/Http/Middleware/EnsureUserIsAdmin.php
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -1,28 +1,35 @@
 <?php
 
+
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class EnsureUserIsAdmin
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // Check if the user is authenticated
         if (!Auth::check()) {
-            return redirect('/login')->with('error', 'You need to be logged in to access this page.');
+            Log::info('User not logged in');
+            return redirect()->route('Auth.login')->with('error', 'You need to be logged in to access this page.');
         }
 
         // Check if the authenticated user has the 'admin' role
         if (Auth::user()->role !== "admin") {
-            return redirect('/products')->with('error', 'You do not have admin access.');
+            Log::info('User tried to access an admin route');
+            return redirect()->route('dashboard.index')->with('error', 'You do not have admin access.');
         }
 
         // Continue to the next request
